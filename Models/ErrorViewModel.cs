04d13cad504834500479f01096013dec03188575
laar99 <l17112363@cdjuarez.tecnm.mx>
2022-08-18 10:01:24 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlysBallon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
